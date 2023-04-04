474e7ac303bf70e243655af23208b2b38a8cc48f
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-04 19:04:13 +0900

    典型90-4

--- educational90/4.cpp
+++ educational90/4.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
 using namespace std;
+#pragma warning (disable: 4996)
 
 int H, W;
 int A[2009][2009];
 int Row[2009], Column[2009];
 int Answer[2009][2009];
 
-int main() {
+int main(){
     cin >> H >> W;
-    for (int i = 1; i <= H; i++){
-        for (int j = 1; j <= W; j++){
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
             scanf("%d", &A[i][j]);
         }
     }
-
-    for (int i = 1; i <= H; i++){
-        for (int j = 1; j <= W; j++){
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
             Row[i] += A[i][j];
-            Column += A[i][j];
+            Column[j] += A[i][j];
         }
     }
-
-    for (int i = 1; i < H; i++){
-        for (int j = 1; j <= W; j++){
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
             Answer[i][j] = Row[i] + Column[j] - A[i][j];
         }
     }
-
-    for (int i = 1; i <= H; i++){
-        for (int j = 1; j <= W; j++){
-            if (j >= 2) printf(" ");
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < W; j++){
+            if (j >= 1) printf(" ");
             printf("%d", Answer[i][j]);
         }
         printf("\n");
     }
-    
     return 0;
 }
