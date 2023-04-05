a09be1579190592b574611ba31ac3871f9e58dd8
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-05 20:03:22 +0900

    典型90-7

--- educational90/7.cpp
+++ educational90/7.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-using namespace std; 
+using namespace std;
 
 const int INF = 2000000000;
 int N;
@@ -9,20 +9,21 @@ int A[300009];
 int Q;
 int B[300009];
 
-int main(){
+int main() {
     cin >> N;
     for (int i = 1; i <= N; i++) cin >> A[i];
     cin >> Q;
-    for (int i = 1;i <= Q; i++) cin >> B[i];
+    for (int i = 1; i <= Q; i++) cin >> B[i];
 
-    sort(A+1, A+N+1);
+    sort(A + 1, A + N + 1);
 
-    for (int i = 1; i <= Q; i++) {
-        int pos1 = lower_bound(A + 1, A + N + 1, B[i]) - A;
+    for (int i = 1; i <= Q; i++){
+        int pos1 = lower_bound(A+1, A+N+1, B[i]) - A;
         int Diff1 = INF, Diff2 = INF;
         if (pos1 <= N) Diff1 = abs(B[i] - A[pos1]);
         if (pos1 >= 2) Diff2 = abs(B[i] - A[pos1 - 1]);
         cout << min(Diff1, Diff2) << endl;
     }
+
     return 0;
 }
