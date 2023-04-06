3d81e11279c696ca5c6d458f7e163022b76fd341
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-06 19:53:40 +0900

    典型90-10

--- educational90/10.cpp
+++ educational90/10.cpp
@@ -6,20 +6,18 @@ int C[100009], P[100009];
 int Q;
 int L[100009], R[100009];
 
-int Sum1[100009];
-int Sum2[100009];
+int Sum1[1000009];
+int Sum2[1000009];
 
 int main() {
-    int tmp[100];
-    for(int i = 0; i < 100; i++) cout << C[i] << endl;
     cin >> N;
     for (int i = 1; i <= N; i++) cin >> C[i] >> P[i];
     cin >> Q;
     for (int i = 1; i <= Q; i++) cin >> L[i] >> R[i];
 
     for (int i = 1; i <= N; i++) {
-        Sum1[i] = Sum1[i-1];
-        Sum2[i] = Sum2[i-1];
+        Sum1[i] = Sum1[i - 1];
+        Sum2[i] = Sum2[i - 1];
         if (C[i] == 1) Sum1[i] += P[i];
         if (C[i] == 2) Sum2[i] += P[i];
     }
