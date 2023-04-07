97ed9d1bd820c7318341f8bc364a04dd80cd036d
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-07 19:54:52 +0900

    [add] 典型90-14

--- educational90/14.cpp
+++ educational90/14.cpp
@@ -4,19 +4,23 @@
 using namespace std;
 
 long long N;
-long long A[100009];
+long long A[100009]
 long long B[100009];
 
 int main() {
-    cin >> N;
-    for (int i = 1; i <= N; i++) cin >> A[i];
-    for (int i = 1; i <= N; i++) cin >> B[i];
-
-    sort(A + 1, A + N + 1);
-    sort(B + 1, B + N + 1);
-
-    long long Answer = 0;
-    for (int i = 1; i <= N; i++) Answer += abs(A[i] - B[i]);
-    cout << Answer << endl;
+   cin >> N;
+   for (int i = 0; i < N; i++){
+    cin >> A[i];
+   } 
+   for (int i = 0; i < N; i++){
+    cin >> B[i];
+   } 
+   sort(A, A + N);
+   sort(B, B + N);
+   long long ans = 0;
+    for (int i = N - 1; i >= 0; i--){
+        ans += abs(A[i] - B[i]);
+    }
+    cout << ans << endl;
     return 0;
 }
