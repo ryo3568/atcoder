cbf6df1da9de493ac7aebff7d4980336498f44e3
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-12 19:30:47 +0900

    典型90-20

--- educational90/20.cpp
+++ educational90/20.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 using namespace std; 
 
 long long A, B, C;
@@ -6,7 +6,7 @@ long long E = 1;
 
 int main() {
     cin >> A >> B >> C;
-    for(int i = 1; i <= B; i++) E *= C;
+    for (int i= 1; i <= B;i++) E *= C;
     if (A < E) cout << "Yes" << endl;
     else cout << "No" << endl;
     return 0;
