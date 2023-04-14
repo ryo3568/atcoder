bdd678e9d4b212e47f473e2505f7cf16b1b86a5b
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-14 18:58:29 +0900

    [add] 典型90-24

--- educational90/24.cpp
+++ educational90/24.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <cmath> 
-using namespace std; 
+#include <cmath>
+using namespace std;
 
 long long N, K;
 long long A[1 << 18], B[1 << 18];
 
 int main() {
     cin >> N >> K;
-    for (int i = 1; i <= N; i++) cin >> A[i];
-    for (int i = 1; i <= N; i++) cin >> B[i];
+    for (int i = 0; i <= N; i++) cin >> A[i];
+    for (int i = 0; i <= N; i++) cin >> B[i];
 
     long long Diff = 0;
     for (int i = 1; i <= N; i++) Diff += abs(A[i] - B[i]);
-    if (Diff > K) {
+    if (Diff > K){
         cout << "No" << endl;
         return 0;
     }
