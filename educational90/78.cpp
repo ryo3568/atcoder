1b9063370b91a0cd4722a7e5959e6613b14cc721
Ryo Yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-04-23 22:03:09 +0900

    典型90-78

---
+++ educational90/78.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> G(N);
+    for(int i=0;i < M;i++){
+        int a, b;
+        cin >> a >> b; --a; b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    int answer = 0;
+    for(int i=0;i < N;i++){
+        int cnt =0;
+        for(int j: G[i]){
+            if(j < i) {
+                ++cnt;
+            }
+        }
+        if(cnt == 0){
+            answer++;
+        }
+    }
+    cout << answer << endl;
+    return 0;
+}
